85ddb6c61645cc95a255227816f848a26e0090b2
Nadolskyi <nadolskyy@i.ua>
2016-09-01 15:26:02 +0000

    1

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 
-var arr = [1,1];
-console.log (arr[0]); 
-for(var i=1; i<100; i++) {arr.push(arr[i]+arr[i-1]);
- console.log (arr[i]);
-}
+
 var a = Number(prompt('Вкажіть порядковий номер ряду чисел Фібоначчі'));
-	
+var arr = [1,1];
+for(var i=1; i<a; i++) {arr.push(arr[i]+arr[i-1]);
+    }
 alert('Значення числа ряду Фібоначчі під порядковим номером '+[a]+' становить '+arr[a] );
